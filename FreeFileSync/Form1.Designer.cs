ee373ab1fa8e467f0382261508de53c7837cd1ba
DevBaddy <warbaddy19@gmail.com>
2022-05-06 19:58:44 -0300

    Added the ability to move files from one location to another

--- FreeFileSync/Form1.Designer.cs
+++ FreeFileSync/Form1.Designer.cs
@@ -30,6 +30,11 @@
         {
             this.filePath1 = new System.Windows.Forms.TextBox();
             this.filePathSearch1 = new System.Windows.Forms.Button();
+            this.fileDisplay1 = new System.Windows.Forms.TextBox();
+            this.fileDisplay2 = new System.Windows.Forms.TextBox();
+            this.filePathSearch2 = new System.Windows.Forms.Button();
+            this.filePath2 = new System.Windows.Forms.TextBox();
+            this.Synchronize = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // filePath1
@@ -49,11 +54,65 @@
             this.filePathSearch1.UseVisualStyleBackColor = true;
             this.filePathSearch1.Click += new System.EventHandler(this.filePathSearch1_Click);
             // 
+            // fileDisplay1
+            // 
+            this.fileDisplay1.BackColor = System.Drawing.SystemColors.Window;
+            this.fileDisplay1.Location = new System.Drawing.Point(72, 213);
+            this.fileDisplay1.Multiline = true;
+            this.fileDisplay1.Name = "fileDisplay1";
+            this.fileDisplay1.ReadOnly = true;
+            this.fileDisplay1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.fileDisplay1.Size = new System.Drawing.Size(506, 258);
+            this.fileDisplay1.TabIndex = 2;
+            // 
+            // fileDisplay2
+            // 
+            this.fileDisplay2.BackColor = System.Drawing.SystemColors.Window;
+            this.fileDisplay2.Location = new System.Drawing.Point(805, 213);
+            this.fileDisplay2.Multiline = true;
+            this.fileDisplay2.Name = "fileDisplay2";
+            this.fileDisplay2.ReadOnly = true;
+            this.fileDisplay2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.fileDisplay2.Size = new System.Drawing.Size(506, 258);
+            this.fileDisplay2.TabIndex = 5;
+            // 
+            // filePathSearch2
+            // 
+            this.filePathSearch2.Location = new System.Drawing.Point(1217, 79);
+            this.filePathSearch2.Name = "filePathSearch2";
+            this.filePathSearch2.Size = new System.Drawing.Size(94, 29);
+            this.filePathSearch2.TabIndex = 4;
+            this.filePathSearch2.Text = "...";
+            this.filePathSearch2.UseVisualStyleBackColor = true;
+            this.filePathSearch2.Click += new System.EventHandler(this.filePathSearch2_Click);
+            // 
+            // filePath2
+            // 
+            this.filePath2.Location = new System.Drawing.Point(805, 81);
+            this.filePath2.Name = "filePath2";
+            this.filePath2.Size = new System.Drawing.Size(416, 27);
+            this.filePath2.TabIndex = 3;
+            // 
+            // Synchronize
+            // 
+            this.Synchronize.Location = new System.Drawing.Point(637, 539);
+            this.Synchronize.Name = "Synchronize";
+            this.Synchronize.Size = new System.Drawing.Size(102, 29);
+            this.Synchronize.TabIndex = 6;
+            this.Synchronize.Text = "Synchronize";
+            this.Synchronize.UseVisualStyleBackColor = true;
+            this.Synchronize.Click += new System.EventHandler(this.Synchronize_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1397, 829);
+            this.Controls.Add(this.Synchronize);
+            this.Controls.Add(this.fileDisplay2);
+            this.Controls.Add(this.filePathSearch2);
+            this.Controls.Add(this.filePath2);
+            this.Controls.Add(this.fileDisplay1);
             this.Controls.Add(this.filePathSearch1);
             this.Controls.Add(this.filePath1);
             this.Name = "Form1";
@@ -67,5 +126,10 @@
 
         private System.Windows.Forms.TextBox filePath1;
         private System.Windows.Forms.Button filePathSearch1;
+        private System.Windows.Forms.TextBox fileDisplay1;
+        private System.Windows.Forms.TextBox fileDisplay2;
+        private System.Windows.Forms.Button filePathSearch2;
+        private System.Windows.Forms.TextBox filePath2;
+        private System.Windows.Forms.Button Synchronize;
     }
 }
--- FreeFileSync/Form1.cs
+++ FreeFileSync/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Windows.Forms;
 
 namespace FreeFileSync
@@ -10,16 +11,82 @@ namespace FreeFileSync
             InitializeComponent();
         }
 
+        string[] files1;
+        string[] files2;
+
         private void filePathSearch1_Click(object sender, EventArgs e)
         {
-            OpenFileDialog fileDialog = new OpenFileDialog();
-            fileDialog.Filter = "All Files (*.*)|*.*";
-            fileDialog.FilterIndex = 1;
-            fileDialog.Multiselect = true;
-
-            if (fileDialog.ShowDialog() == DialogResult.OK)
+            if (filePath1.Text != "")
             {
-                filePath1.Text = fileDialog.FileName;  
+                filePath1.Text = "";
+            }
+
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                filePath1.Text = folderBrowserDialog.SelectedPath;
+                getPathFiles(folderBrowserDialog.SelectedPath, filePath1.Name);
+            }
+        }
+
+        private void filePathSearch2_Click(object sender, EventArgs e)
+        {
+            if (filePath2.Text != "")
+            {
+                filePath2.Text = "";
+            }
+
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                filePath2.Text = folderBrowserDialog.SelectedPath;
+                getPathFiles(folderBrowserDialog.SelectedPath, filePath2.Name);
+            }
+        }
+
+        private void getPathFiles(string path, string textBox)
+        {
+            int i1 = 0;
+            int i2 = 0;
+
+            if (textBox == "filePath1")
+            {
+                files1 = Directory.GetFiles(path);
+
+                foreach (var file in files1)
+                {
+                    fileDisplay1.Text += $"{files1[i1]}\r\n";
+                    i1++;
+                }
+            }
+
+            if (textBox == "filePath2")
+            {
+                files2 = Directory.GetFiles(path);
+
+                foreach (var file in files2)
+                {
+                    fileDisplay2.Text += $"{files2[i2]}\r\n";
+                    i2++;
+                }
+            }
+        }
+
+        private void Synchronize_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+
+            if (files1 != null && files2 != null)
+            {
+                foreach (var file in files1)
+                {
+                    string filename = Path.GetFileName(files1[i]);
+
+                    File.Move($"{files1[i]}", $"{filePath2.Text}\\{filename}");
+                    i++;
+                }
             }
         }
     }
